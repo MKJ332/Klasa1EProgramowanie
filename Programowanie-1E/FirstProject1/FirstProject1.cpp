6e25ce97a943cca947353cb8c6a90348d378ed1b
karol.szkola <karol.szkola@ezse.pl>
2022-09-28 09:56:14 +0200

    t

--- Programowanie-1E/FirstProject1/FirstProject1.cpp
+++ Programowanie-1E/FirstProject1/FirstProject1.cpp
@@ -1,9 +1,31 @@
-﻿// FirstProject1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿
+// FirstProject.cpp : This file contains the 'main' function.
+//  Program execution begins and ends there.
+
 
 #include <iostream>
 
 int main()
 {
+    
+    std::cout << "Hel\nlo World!\n";
     std::cout << "Hello World!\n";
-}
+
+    std::cout << "Predkosc to km\h";
+
+    std::cout << "Tabulator\ttabulator";
+
+
+    std::cout << -56;
+    std::cout << "\n";
+    std::cout << "56";
+    std::cout << "\n";
+    std::cout << +15.5;
+    std::cout << "\n";
+    
+
+    std::cout << "podaj liczbe: \n";
+    int numberFromUser;
+    std::cin >> numberFromUser;
+    std::cout << "Podano: " << numberFromUser << "\n";
+ } 
